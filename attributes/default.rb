b51a27c6e731721634ee3c2e1cf269d30148ba2a
Theo Cushion <theo@jivatechnology.com>
2012-02-28 16:59:43 +0000

    Feature: Added sunspot schema into recipe so that solr can run before the application has been deployed, it will then update the config after the application has deployed

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,13 +3,17 @@ include_attribute "jetty"
 expand!
 
 default[:solr][:version]   = "1.4.1"
-default[:solr][:link]      = "http://apache.favoritelinks.net/lucene/solr/#{solr.version}/apache-solr-#{solr.version}.tgz"
+default[:solr][:link]      = "http://www.mirrorservice.org/sites/ftp.apache.org/lucene/solr/#{solr.version}/apache-solr-#{solr.version}.tgz"
 default[:solr][:checksum]  = "d795bc477335b3e29bab7073b385c93fca4be867aae345203da0d1e438d7543f"
 default[:solr][:directory] = "/usr/local/src"
 default[:solr][:download]  = "#{solr.directory}/apache-solr-#{solr.version}.tgz"
 default[:solr][:extracted] = "#{solr.directory}/apache-solr-#{solr.version}"
 default[:solr][:war]       = "#{solr.extracted}/dist/apache-solr-#{solr.version}.war"
 
-default[:solr][:context_path] = 'solr'
-default[:solr][:home]         = "#{node.jetty.home}/webapps/#{node.solr.context_path}"
-default[:solr][:data]         = "#{node.jetty.home}/webapps/#{node.solr.context_path}/data"
+default[:solr][:context_path]  = 'solr'
+default[:solr][:home]          = "/etc/solr"
+set[:solr][:config]            = node.solr.home + "/conf"
+set[:solr][:lib]               = node.solr.home + "/lib"
+default[:solr][:data]          = "#{node.jetty.home}/webapps/#{node.solr.context_path}/data"
+default[:solr][:custom_config] = nil
+default[:solr][:custom_lib]    = nil
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,9 +7,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -48,3 +48,74 @@ template "#{node.jetty.home}/contexts/solr.xml" do
   source "solr.context.erb"
   notifies :restart, resources(:service => "jetty")
 end
+
+remote_directory node.solr.config do
+  source       "sunspot-1.2.1"
+  owner        node.jetty.user
+  group        node.jetty.group
+  files_owner  node.jetty.user
+  files_group  node.jetty.group
+  files_backup 0
+  files_mode   "644"
+  purge        true
+
+  notifies     :restart, resources(:service => "jetty"), :immediately
+  not_if       { File.exists? node.solr.config }
+end
+
+
+if node.solr.custom_lib
+
+  bash "Copy custom lib to solr" do
+    code <<-EOH
+      rm -rf #{node.solr.lib}
+      cp -r #{node.solr.custom_lib} #{node.solr.lib}
+      chown -R #{node.jetty.user}:#{node.jetty.group} #{node.solr.lib}
+      find #{node.solr.lib} -type f -exec chmod 640 \\;
+      find #{node.solr.lib} -type d -exec chmod 750 \\;
+    EOH
+    notifies     :restart, resources(:service => "jetty"), :immediately
+    # Only copy the lib if it exists, and it is different from what is already there
+    only_if <<-EOH
+      test -e #{node.solr.custom_lib} &&
+      ( diff -r #{node.solr.custom_lib} #{node.solr.lib}; test $? != 0 )
+    EOH
+  end
+
+end
+
+if node.solr.custom_config
+
+  bash "Copy custom config to solr" do
+    code <<-EOH
+      rm -rf #{node.solr.config}
+      cp -r #{node.solr.custom_config} #{node.solr.config}
+      chown -R #{node.jetty.user}:#{node.jetty.group} #{node.solr.config}
+      find #{node.solr.config} -type f -exec chmod 640 \\;
+      find #{node.solr.config} -type d -exec chmod 750 \\;
+    EOH
+    notifies     :restart, resources(:service => "jetty"), :immediately
+    # Only copy the config if it exists, and it is different from what is already there
+    only_if <<-EOH
+      test -e #{node.solr.custom_config}/solrconfig.xml &&
+      ( diff -r #{node.solr.custom_config} #{node.solr.config}; test $? != 0 )
+    EOH
+  end
+
+end
+
+remote_directory "/etc/solr/conf" do
+  source       "sunspot-1.2.1"
+  owner        node.jetty.user
+  group        node.jetty.group
+  files_owner  node.jetty.user
+  files_group  node.jetty.group
+  files_backup 0
+  files_mode   "644"
+  purge        true
+  notifies     :restart, resources(:service => "jetty")
+  not_if       "test -e #{node.solr.custom_config}/solrconfig.xml"
+end
+
+
+
