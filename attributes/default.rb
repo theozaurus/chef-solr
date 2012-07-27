3baba5227418888a3e091a071180226127d2c7b5
Theo Cushion <theo@jivatechnology.com>
2012-07-27 13:48:28 +0100

    Feature: Updated solr to 3.6.1

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,9 +2,9 @@ include_attribute "jetty"
 
 expand!
 
-default[:solr][:version]   = "3.5.0"
+default[:solr][:version]   = "3.6.1"
 default[:solr][:link]      = "http://www.mirrorservice.org/sites/ftp.apache.org/lucene/solr/#{solr.version}/apache-solr-#{solr.version}.tgz"
-default[:solr][:checksum]  = "804f3ba9d1296f81388605a79538b7362355693fbdd03b7b2dbf9a706bf1d1d0" #sha265
+default[:solr][:checksum]  = "1b4552ba95c8456d4fbd596e82028eaa0619b6942786e98e1c4c31258543c708" #sha265
 default[:solr][:directory] = "/usr/local/src"
 default[:solr][:download]  = "#{solr.directory}/apache-solr-#{solr.version}.tgz"
 default[:solr][:extracted] = "#{solr.directory}/apache-solr-#{solr.version}"
