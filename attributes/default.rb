4f2ec23091be44ec74d00af03067ed249139ec75
Jason Byck <jasonbyck@gmail.com>
2013-05-14 11:31:38 -0400

    Add Solr 4 specific default attributes.

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,12 +3,23 @@ include_attribute "jetty"
 expand!
 
 default[:solr][:version]   = "3.6.1"
-default[:solr][:link]      = "http://www.mirrorservice.org/sites/ftp.apache.org/lucene/solr/#{solr.version}/apache-solr-#{solr.version}.tgz"
-default[:solr][:checksum]  = "1b4552ba95c8456d4fbd596e82028eaa0619b6942786e98e1c4c31258543c708" #sha265
-default[:solr][:directory] = "/usr/local/src"
-default[:solr][:download]  = "#{solr.directory}/apache-solr-#{solr.version}.tgz"
-default[:solr][:extracted] = "#{solr.directory}/apache-solr-#{solr.version}"
-default[:solr][:war]       = "#{solr.extracted}/dist/apache-solr-#{solr.version}.war"
+
+if default[:solr][:version].split('.').first.to_i >= 4
+  default[:solr][:link]      = "http://www.mirrorservice.org/sites/ftp.apache.org/lucene/solr/#{solr.version}/solr-#{solr.version}.tgz"
+  default[:solr][:checksum]  = "1b4552ba95c8456d4fbd596e82028eaa0619b6942786e98e1c4c31258543c708" #sha265
+  default[:solr][:directory] = "/usr/local/src"
+  default[:solr][:download]  = "#{solr.directory}/solr-#{solr.version}.tgz"
+  default[:solr][:extracted] = "#{solr.directory}/solr-#{solr.version}"
+  default[:solr][:war]       = "#{solr.extracted}/dist/solr-#{solr.version}.war"
+else
+  default[:solr][:link]      = "http://www.mirrorservice.org/sites/ftp.apache.org/lucene/solr/#{solr.version}/apache-solr-#{solr.version}.tgz"
+  default[:solr][:checksum]  = "1b4552ba95c8456d4fbd596e82028eaa0619b6942786e98e1c4c31258543c708" #sha265
+  default[:solr][:directory] = "/usr/local/src"
+  default[:solr][:download]  = "#{solr.directory}/apache-solr-#{solr.version}.tgz"
+  default[:solr][:extracted] = "#{solr.directory}/apache-solr-#{solr.version}"
+  default[:solr][:war]       = "#{solr.extracted}/dist/apache-solr-#{solr.version}.war"
+end
+
 
 default[:solr][:context_path]  = 'solr'
 default[:solr][:home]          = "/etc/solr"
