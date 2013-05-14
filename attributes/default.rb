55fe3c6d445deead85936926e783d0b0c06af10e
Jason Byck <jasonbyck@gmail.com>
2013-05-14 12:24:49 -0400

    Fix check for Solr version

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,7 +6,7 @@ default[:solr][:version]   = "3.6.1"
 default[:solr][:directory] = "/usr/local/src"
 default[:solr][:checksum]  = "1b4552ba95c8456d4fbd596e82028eaa0619b6942786e98e1c4c31258543c708" #sha265
 
-if default[:solr][:version].split('.').first.to_i >= 4
+if solr.version.split('.').first.to_i >= 4
   default[:solr][:link]      = "http://www.mirrorservice.org/sites/ftp.apache.org/lucene/solr/#{solr.version}/solr-#{solr.version}.tgz"
   default[:solr][:download]  = "#{solr.directory}/solr-#{solr.version}.tgz"
   default[:solr][:extracted] = "#{solr.directory}/solr-#{solr.version}"
